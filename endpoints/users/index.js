b90f8f62d128257f119a85eb70c2fcbb597dd92c
leandjb <leandjb@gmail.com>
2024-11-19 18:18:09 -0500

    refactor for HTTP methods

--- endpoints/users/index.js
+++ endpoints/users/index.js
@@ -1,6 +1,4 @@
-const axios = require('axios');
-
-const handlers = {
+const handlers = ({ axios }) => ({
 
     get: async (req, res) => {
         const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
@@ -8,17 +6,28 @@ const handlers = {
         res.status(200).send(data);
     },
 
-    post: async () => {
-
+    post: async (req, res) => {
+        const { body } = req;
+        const { data } = await axios.post('https://jsonplaceholder.typicode.com/users', body);
+    
+        res.status(201).send(data);
     },
 
-    put: async () => {
-
+    put: async (req, res) => {
+        const { body } = req;
+        const { id } = req.params;
+        await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`, body);
+    
+        res.sendStatus(204);
     },
 
-    delete: async () => {
-
+    delete:async (req, res) => {
+        const { id } = req.params;
+        await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+    
+        res.sendStatus(204);
     }
-}
+
+})
 
 module.exports = handlers
--- server.js
+++ server.js
@@ -5,34 +5,17 @@ const users = require('./endpoints/users');
 const app = express();
 const port = 3000;
 
+
 app.use(parser.urlencoded({ extended: false }));
 app.use(parser.json());
 
 
+userHandlers = users({ axios })
 
-app.get('/', users.get);
-
-app.post('/', async (req, res) => {
-    const { body } = req;
-    const { data } = await axios.post('https://jsonplaceholder.typicode.com/users', body);
-
-    res.status(201).send(data);
-});
-
-app.put('/:id', async (req, res) => {
-    const { body } = req;
-    const { id } = req.params;
-    await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`, body);
-
-    res.sendStatus(204);
-});
-
-app.delete('/:id', async (req, res) => {
-    const { id } = req.params;
-    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
-
-    res.sendStatus(204);
-});
+app.get('/', userHandlers.get);
+app.post('/', userHandlers.post);
+app.put('/:id', userHandlers.put);
+app.delete('/:id', userHandlers.delete);
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
