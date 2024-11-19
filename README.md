54717175a2ef1f3b7fbdc5eede6c2bcd28bc4291
leandjb <leandjb@gmail.com>
2024-11-19 13:58:08 -0500

    added GET and POST methods. LB

---
+++ README.md
@@ -0,0 +1,16 @@
+# API TESTING using JEST
+
+## Instalation:
+```
+    yarn install
+```
+
+## Run Node-Express Server:
+````
+    node index.js
+```
+
+## Excecute JEST Test Suite:
+```
+    .
+```
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const axios = require('axios')
+const parser = require('body-parser')
+const app = express()
+const port = 3000
+
+
+
+app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json());
+
+
+app.get('/', async (req, res) => {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
+    
+    res.send(data);
+})
+
+app.post('/', async (req, res) => {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+    res.send(data);
+    // res.sendStatus(200);
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+})
